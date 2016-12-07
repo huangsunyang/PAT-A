255c67c9c14f211c80bfc5bebe40dadc8a4b9fbf
huangsungyang <huangsunyang@126.com>
2016-12-07 16:54:32 +0800

    update

--- 1118/main.cpp
+++ 1118/main.cpp
@@ -7,75 +7,53 @@
 //
 
 #include <iostream>
-#include <vector>
+#include <cstdio>
+#include <set>
+#include <unordered_set>
 using namespace std;
 
-int find_first_bird(int * birds, int k) {
-    if (birds[k] != k) {
-        birds[k] = find_first_bird(birds, birds[k]);
+int ar[10005];
+
+int find_father(int a) {
+  if (ar[a] == -1) return a;
+  else ar[a] = find_father(ar[a]);
+  return ar[a];
+}
+
+void merge(int a, int b) {
+  int c = find_father(a);
+  int d = find_father(b);
+  ar[d] = c;
+}
+
+int main() {
+  for (size_t i = 0; i < 10005; i++) {
+    ar[i] = -1;
+  }
+  int n, q;
+  unordered_set<int> st, ed;
+  cin >> n;
+  for (size_t i = 0; i < n; i++) {
+    int temp_num, temp;
+    cin >> temp_num;
+    int head_bird;
+    cin >> head_bird;
+    st.insert(head_bird);
+    for (size_t j = 1; j < temp_num; j++) {
+      cin >> temp;
+      st.insert(temp);
+      merge(temp, head_bird);
     }
-    return birds[k];
-}
-
-void link(int * birds, int a, int b) {
-    int p = find_first_bird(birds, a);
-    int q = find_first_bird(birds, b);
-    birds[p] = q;
-}
-
-int main(int argc, const char * argv[]) {
-    int birds[20003];
-    int trees[20003];
-    vector<int> vec;
-    int n, m, l, temp, temp1, tree_num = 0, max_birds = 0;
+  }
+  for (size_t i = 1; i <= st.size(); i++) {
+    ed.insert(find_father(i));
+  }
+  cout << ed.size() << ' '<<st.size()<<endl;
+  cin >> q;
+  for (size_t i = 0; i < q; i++) {
     int a, b;
-    //初始化
-    for (int i = 0; i < 20003; i++) {
-        trees[i] = 0;
-        birds[i] = -1;
-    }
-    scanf("%d", &n);    //图片数量
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &m);    //图片中鸟的数量
-        for(int j = 0; j < m; j++) {
-            scanf("%d", &temp);
-            vec.push_back(temp);    //先保存起来再说
-        }
-        temp1 = vec[0];
-        for (int j = 0; j < m; j++) {
-            if (birds[vec[j]] != -1) {
-                temp1 = birds[vec[j]];
-                break;                  //找到一个有父节点的，找不到就任意找一个即可
-            }
-        }
-        for (int j = 0; j < m; j++) {
-            if(birds[vec[j]] == -1)
-                birds[vec[j]] = temp1;      //原来没有指过，那么指向一只鸟王
-            else {                          //原来已经在另一个集合中
-                link(birds, vec[j], vec[0]);
-            }
-        }
-        vec.clear();
-    }
-    for (int i = 1; i < 20003; i++) {
-        if (birds[i] != -1) {
-            int k = find_first_bird(birds, i);      //找到每一只鸟的鸟王
-            trees[k]++;                             //一只鸟王代表一棵树。这棵树上的鸟的数量加1
-        } else  {
-            max_birds = i - 1;                      //说明编号到此结束了
-            break;
-        }
-    }
-    for (int i = 1; i <= max_birds; i++) {
-        if(trees[i] != 0) {                     //有多少棵树上有鸟，代表就有多少棵树
-            tree_num++;
-        }
-    }
-    printf("%d %d\n", tree_num, max_birds);
-    scanf("%d", &l);
-    for (int i = 0; i < l; i++) {
-        scanf("%d %d", &a, &b);
-        printf("%s", birds[a] == birds[b]  ? "Yes\n" : "No\n"); //鸟王一样说明在一棵树上
-    }
-    return 0;
+    cin >> a >> b;
+    printf("%s\n", find_father(a) == find_father(b) ? "Yes" : "No");
+  }
+  return 0;
 }
